b91db02273962c638ac4702da529a1d331c02e9e
Synzenn <velriel42@gmail.com>
2025-10-31 10:32:26 +0700

    membuat divider untuk jaraknya

--- app/src/main/java/com/example/tugasinput/TeksEdit.kt
+++ app/src/main/java/com/example/tugasinput/TeksEdit.kt
@@ -96,4 +96,10 @@ fun FormDataDiri(modifier: Modifier
             }
         )
 
-}
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource( id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
