983a300a9d1c3f348ee75fefb33fa1119dfcceff
Synzenn <velriel42@gmail.com>
2025-10-31 10:31:24 +0700

    membuat radio button yg berisi selected dan onclick

--- app/src/main/java/com/example/tugasinput/TeksEdit.kt
+++ app/src/main/java/com/example/tugasinput/TeksEdit.kt
@@ -71,5 +71,17 @@ fun FormDataDiri(modifier: Modifier
                     selected = textJK == item,
 
                     onClick = { textJK = item}
-                ),
+                ), verticalAlignment = Alignment.CenterVertically  ){
+                    RadioButton(
+                        selected = textJK == item,
+
+                        onClick = {
+                            textJK = item
+                        }
+                    )
+                    Text(item)
+                }
+            }
+        }
+
 }
